90062fd02e3f90900d1c8054e936d9f9db124dd3
Filip <Filipzstio1@interia.pl>
2023-04-26 19:08:11 +0200

    Initial commit

---
+++ src/main/java/com/example/projektkonc/ProjektKoncApplication.java
@@ -0,0 +1,13 @@
+package com.example.projektkonc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektKoncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektKoncApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/projektkonc/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.projektkonc.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository <User, Integer> {
+
+}
